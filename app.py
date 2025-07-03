c262843914ede9d9685ac955ed12b22858069907
gkshiang <gkshiang3.14@gmail.com>
2025-07-03 11:06:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.title ("Dell Global Busyness Centre")
 st.image ("dell_logo.jpg", width = 300)
 st.camera_input ("Case Input:")
 st.date_input ("Transactional Date")
-st.radio ("Department:", ['AI', 'CFI', 'ISG NPI', 'CSG NPI']
+st.radio ("Department:", ['AI', 'CFI', 'ISG NPI', 'CSG NPI'])
 
 
 ###
