27311e01c30b83f44f3950fc1b2a4e7663dacb78
gkshiang <gkshiang3.14@gmail.com>
2025-07-03 11:06:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
 st.title ("Dell Global Busyness Centre")
-st.image ("dell_logo.jpg")
-st.camera_input ("Case")
+st.image ("dell_logo.jpg", width = 300)
+st.camera_input ("Case Input:")
+st.date_input ("Transactional Date")
+st.radio ("Department:", ['AI', 'CFI', 'ISG NPI', 'CSG NPI']
+
+
+###
 
